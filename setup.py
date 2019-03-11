e4d32d911ce38ef9caa4414df8bb3653eca77ee6
chachi <chachi@debian.!!!>
2019-03-11 10:39:39 -0400

    First commit using https://drivendata.github.io/cookiecutter-data-science/

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Example Uber take-home challenge',
+    author='Craig Paulette',
+    license='MIT',
+)
